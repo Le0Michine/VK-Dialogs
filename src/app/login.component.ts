11c6f9eec9face4b882a9589bf2f3d7cdb2c70ba
Leo <leomichine@gmail.com>
2016-10-23 20:57:03 +0200

    fixed indents

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -3,13 +3,13 @@ import { Router } from "@angular/router";
 import { VKService } from "./vk-service";
 
 @Component({
-  selector: "login",
-  templateUrl: "login.component.html",
-  styleUrls: [
-      "login.component.css",
-      "css/color-scheme.css",
-      "css/font-style.css"
-     ]
+    selector: "login",
+    templateUrl: "login.component.html",
+    styleUrls: [
+        "login.component.css",
+        "css/color-scheme.css",
+        "css/font-style.css"
+       ]
 })
 export class LoginComponent {
     constructor(private router: Router, private vkservice: VKService) { }
