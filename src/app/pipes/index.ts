a03560352f49b665b6e9807a1f1b74021ad2e116
Leo <leomichine@gmail.com>
2016-11-26 17:12:00 +0100

    removed pipe

--- src/app/pipes/index.ts
+++ src/app/pipes/index.ts
@@ -3,7 +3,6 @@ export * from "./chat-action.pipe";
 export * from "./cut-links.pipe";
 export * from "./format-date.pipe";
 export * from "./link-to-user.pipe";
-export * from "./message.pipe";
 export * from "./safe.pipe";
 export * from "./sticker.pipe";
 export * from "./emoji.pipe";
@@ -13,7 +12,6 @@ import { ChatActionPipe } from "./chat-action.pipe";
 import { CutLinksPipe } from "./cut-links.pipe";
 import { FormatDatePipe } from "./format-date.pipe";
 import { LinkToUserPipe } from "./link-to-user.pipe";
-import { ReversePipe } from "./message.pipe";
 import { SafeHtmlPipe, SafeStylePipe } from "./safe.pipe";
 import { StickerPipe } from "./sticker.pipe";
 import { EmojiPipe } from "./emoji.pipe";
@@ -25,7 +23,6 @@ export const PIPES = [
     MessageAttachmentSubTitlePipe,
     MessageAttachmentTitlePipe,
     MessageAttachmentUrlPipe,
-    ReversePipe,
     LinkToUserPipe,
     FormatDatePipe,
     CutLinksPipe,
