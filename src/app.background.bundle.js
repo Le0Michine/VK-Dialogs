b12599b463fdd5ef63ac3ff38d1a3289307699b3
Le0Michine <leomichine@gmail.com>
2017-04-14 23:01:40 +0200

    added bundle js files

---
+++ src/app.background.bundle.js
@@ -0,0 +1,5 @@
+import '../dist/app.background/inline.bundle.js';
+import '../dist/app.background/polyfills.bundle.js';
+import '../dist/app.background/styles.bundle.js';
+import '../dist/app.background/vendor.bundle.js';
+import '../dist/app.background/main.bundle.js';
---
+++ src/app.install.bundle.js
@@ -0,0 +1,5 @@
+import '../dist/app.install/inline.bundle.js';
+import '../dist/app.install/polyfills.bundle.js';
+import '../dist/app.install/styles.bundle.js';
+import '../dist/app.install/vendor.bundle.js';
+import '../dist/app.install/main.bundle.js';
